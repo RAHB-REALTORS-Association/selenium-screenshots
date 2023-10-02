39723c187fbb0aec1e0cbed89953cb3fe395625f
Justin Hayes <justinh@rahb.ca>
2023-10-02 15:30:08 -0400

    chromedriver settings

--- resources/screenshot.py
+++ resources/screenshot.py
@@ -37,6 +37,8 @@ class ScreenshotAPI(Resource):
         # Setup Selenium with headless Chrome
         chrome_options = Options()
         chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument(f"--window-size={width},{height}")
         
         driver = webdriver.Chrome(options=chrome_options)
