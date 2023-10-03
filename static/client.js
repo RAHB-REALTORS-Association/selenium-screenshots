a06720ecd2a0be05feeb9da7d1dd00e8994846a4
Justin Hayes <52832301+justinh-rahb@users.noreply.github.com>
2023-10-03 00:12:12 -0400

    spinners

--- static/client.js
+++ static/client.js
@@ -1,47 +1,65 @@
-$("#submitButton").click(function(e) {
-    e.preventDefault();
+$(document).ready(function() {
     
-    // Get input values
-    const apiUrl = $("#apiUrl").val();
-    const apiKey = $("#apiKey").val();
-    const websiteUrl = encodeURIComponent($("#websiteUrl").val());
-    const viewport = $("#viewport").val();
-    const format = $("#format").val();
-    const delay = $("#delay").val();
-
-    // Construct the request URL and headers
-    const requestUrl = `${apiUrl}/screenshot?url=${websiteUrl}&viewport=${viewport}&format=${format}&delay=${delay}`;
-    const headers = {
-        "Authorization": `Bearer ${apiKey}`
-    };
-
-    // Make the AJAX GET request to the screenshot API
-    $.ajax({
-        url: requestUrl,
-        type: "GET",
-        headers: headers,
-        xhrFields: {
-            responseType: 'blob'
-        },
-        success: function(blob) {
-            const imageUrl = URL.createObjectURL(blob);
-            $("#resultContainer").html('<img src="' + imageUrl + '" style="display: none;" />');
-            $("#resultContainer img").fadeIn();
-        },
-        error: function(error) {
-            $("#resultContainer").html('<span style="color: red;">Error taking screenshot.</span>');
-        },
-        complete: function() {
-            // Handle spinner and button state after request completion
-            // (assuming you have a spinner element with ID "spinner")
-            $("#spinner").hide();
-            $("#submitButton").prop("disabled", false);
-        }
+    // Function to trigger the download of the image
+    $("#saveButton").click(function(e) {
+        e.preventDefault();
+        $("#saveSpinner").show();  // Show the spinner
+        
+        let imageUrl = $("#resultContainer img").attr('src');
+        let link = document.createElement('a');
+        link.href = imageUrl;
+        link.download = 'screenshot.png';  // You can customize the filename here
+        link.click();
+        
+        $("#saveSpinner").hide();  // Hide the spinner
     });
 
-    // Save the API settings to local storage if checkbox is checked
-    if ($("#saveSettingsCheckbox").is(":checked")) {
-        localStorage.setItem("apiUrl", apiUrl);
-        localStorage.setItem("apiKey", apiKey);
-    }
+    // Function to handle the screenshot request
+    $("#submitButton").click(function(e) {
+        e.preventDefault();
+        $("#screenshotSpinner").show();  // Show the spinner
+        $("#submitButton").prop("disabled", true);
+        
+        // Get input values
+        const apiUrl = $("#apiUrl").val();
+        const apiKey = $("#apiKey").val();
+        const websiteUrl = encodeURIComponent($("#websiteUrl").val());
+        const viewport = $("#viewport").val();
+        const format = $("#format").val();
+        const delay = $("#delay").val();
+
+        // Construct the request URL and headers
+        const requestUrl = `${apiUrl}?url=${websiteUrl}&viewport=${viewport}&format=${format}&delay=${delay}`;
+        const headers = {
+            "Authorization": `Bearer ${apiKey}`
+        };
+
+        // Make the AJAX GET request to the screenshot API
+        $.ajax({
+            url: requestUrl,
+            type: "GET",
+            headers: headers,
+            xhrFields: {
+                responseType: 'blob'
+            },
+            success: function(blob) {
+                const imageUrl = URL.createObjectURL(blob);
+                $("#resultContainer").html('<img src="' + imageUrl + '" style="display: none;" />');
+                $("#resultContainer img").fadeIn();
+            },
+            error: function(error) {
+                $("#resultContainer").html('<span style="color: red;">Error taking screenshot.</span>');
+            },
+            complete: function() {
+                $("#screenshotSpinner").hide();  // Hide the spinner
+                $("#submitButton").prop("disabled", false);
+            }
+        });
+
+        // Save the API settings to local storage if checkbox is checked
+        if ($("#saveSettingsCheckbox").is(":checked")) {
+            localStorage.setItem("apiUrl", apiUrl);
+            localStorage.setItem("apiKey", apiKey);
+        }
+    });
 });
