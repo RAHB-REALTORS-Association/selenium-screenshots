afc58f320746e38278a9be400bcaec65fcc809f7
Justin Hayes <justinh@rahb.ca>
2023-10-22 18:34:42 -0400

    installable PWA

--- docs/client.js
+++ docs/client.js
@@ -222,4 +222,15 @@ document.addEventListener("DOMContentLoaded", function() {
             behavior: 'smooth'
         });
     });
+
+    // Register the service worker
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function() {
+            navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }, function(err) {
+                console.log('ServiceWorker registration failed: ', err);
+            });
+        });
+    }    
 });
---
+++ docs/service-worker.js
@@ -0,0 +1,56 @@
+// service-worker.js
+
+const CACHE_NAME = 'selenium-screenshots-v1';
+const urlsToCache = [
+  './',
+  'styles.css?v=',
+  'client.js?v=',
+  'favicon-16x16.png',
+  'favicon-32x32.png',
+  'favicon-48x48.png',
+  'apple-touch-icon-180x180.png',
+  'android-chrome-192x192.png',
+  'android-chrome-512x512.png',
+  'selenium-screenshots.png',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js',
+  'https://cdn.jsdelivr.net/npm/axios@1.5.1/dist/axios.min.js'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
